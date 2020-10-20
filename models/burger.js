4a1b005bd9f6626d96198296cdc3e5c54ccc862b
Haynes Brotherton <hbrotherton44@gmail.com>
2020-10-20 18:52:13 -0400

    added burger.js

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+// Import the ORM
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(value, cb) {
+    orm.insertOne("burgers", value, function(res) {
+      cb(res);
+    });
+  },
+  update: function(condition, id, cb) {
+    condition = "devoured = true"
+    orm.updateOne("burgers", condition, id, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
